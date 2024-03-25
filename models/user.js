b53401e6598af6b2d959ce9c2a261bc40f9e54d3
Samuel Myung <shmyung4@gmail.com>
2024-03-25 11:28:12 -0700

    Worked on register and authenticate in user.js

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { BCRYPT_WORK_FACTOR } = require("../config.js")
+
 /** User of the site. */
 
 class User {
@@ -9,12 +11,34 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    const hashedPassword = await bcrypt.hash(
+      password, BCRYPT_WORK_FACTOR);
+    const result = await db.query(
+      `INSERT INTO users (username, password, first_name, last_name, phone)
+        VALUES
+          ($1, $2, $3, $4, $5)
+        RETURNING username`,
+      [username, hashedPassword, first_name, last_name, phone]);
+
+    return res.json(result.rows[0]);
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+    const result = await db.query(
+      `SELECT password
+        FROM users
+        WHERE username = $1`
+      [username]);
+    const user = result.rows[0];
+
+    if (user) {
+      return await bcrypt.compare(password, user.password);
+    }
+    throw new UnauthorizedError("Invalid user/password");
   }
+
 
   /** Update last_login_at for user */
 
